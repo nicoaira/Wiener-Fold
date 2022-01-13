0dc070a97971e14e841f3958743020be8a2dca56
nicoaira <aira.nicolas94@gmail.com>
2022-01-13 14:01:12 -0300

    Now it filters negative dG values

--- sort_df.py
+++ sort_df.py
@@ -6,5 +6,11 @@ import os
 print(os.listdir()[0])
 
 df = pd.read_csv('deltaG.csv')
+positive_dG = df['dG'] >= 0
+df = df[positive_dG]
 df = df.sort_values(by=['num_structures','dG'], ascending = [True, False])
+
+
+
+
 df.to_csv('deltaG_sorted.csv', index = False)
